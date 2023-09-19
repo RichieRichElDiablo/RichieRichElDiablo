e8b02d88b0372746a8114e7366706f8c764d82e6
RichieRichElDiablo <145433115+RichieRichElDiablo@users.noreply.github.com>
2023-09-19 03:48:58 -0400

    Create READMe NOW, PLEASE

---
+++ README.m
@@ -0,0 +1,5 @@
+- 666 I’m @RichieRichElDiablo
+RichieRichElDiablo/RichieRichElDiablo is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the link to discover.
+--->
+host.com
